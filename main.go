d4f33559f002d7e9fd9c7f553d0d6d55a175d0de
Evgenii Savelov <igluko@gmail.com>
2025-04-01 07:34:59 +0000

    feat: добавлена поддержка отдельного чата для администратора и скрытие стартового сообщения от общего чата

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// ВАЖНО: используйте `go get` чтобы убедиться, что все библиотеки установлены
+
 package main
 
 import (
@@ -17,8 +19,9 @@ import (
 )
 
 var (
-	lastMonth string
-	siteError bool
+	lastMonth  string
+	siteError  bool
+	firstCheck = true
 )
 
 var insecureTransport = &http.Transport{
@@ -51,10 +54,11 @@ func main() {
 	_ = godotenv.Load()
 	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 	chatID := os.Getenv("TELEGRAM_CHAT_ID")
+	adminChatID := os.Getenv("ADMIN_CHAT_ID")
 
 	go func() {
 		for {
-			checkMonth(botToken, chatID)
+			checkMonth(botToken, chatID, adminChatID)
 			time.Sleep(5 * time.Minute)
 		}
 	}()
@@ -62,12 +66,12 @@ func main() {
 	listenForMessages(botToken)
 }
 
-func checkMonth(botToken, chatID string) {
+func checkMonth(botToken, chatID, adminChatID string) {
 	resp, err := insecureClient.Get("https://www.atsenergo.ru/results/market/calcfacthour")
 	if err != nil {
-		if !siteError && chatID != "" {
+		if !siteError && adminChatID != "" {
 			siteError = true
-			sendTelegramMessage(botToken, chatID, fmt.Sprintf("Ошибка при запросе сайта: %v", err))
+			sendTelegramMessage(botToken, adminChatID, fmt.Sprintf("Ошибка при запросе сайта: %v", err))
 		}
 		return
 	}
@@ -75,43 +79,50 @@ func checkMonth(botToken, chatID string) {
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		if !siteError && chatID != "" {
+		if !siteError && adminChatID != "" {
 			siteError = true
-			sendTelegramMessage(botToken, chatID, fmt.Sprintf("Ошибка при парсинге HTML: %v", err))
+			sendTelegramMessage(botToken, adminChatID, fmt.Sprintf("Ошибка при парсинге HTML: %v", err))
 		}
 		return
 	}
 
 	firstOption := doc.Find("select.form-select.periods option").First()
 	if firstOption == nil {
-		if !siteError && chatID != "" {
+		if !siteError && adminChatID != "" {
 			siteError = true
-			sendTelegramMessage(botToken, chatID, "Не удалось найти текущий месяц на сайте.")
+			sendTelegramMessage(botToken, adminChatID, "Не удалось найти текущий месяц на сайте.")
 		}
 		return
 	}
 
 	newMonth := strings.TrimSpace(firstOption.Text())
 	if newMonth == "" {
-		if !siteError && chatID != "" {
+		if !siteError && adminChatID != "" {
 			siteError = true
-			sendTelegramMessage(botToken, chatID, "Не удалось определить значение первого месяца.")
+			sendTelegramMessage(botToken, adminChatID, "Не удалось определить значение первого месяца.")
 		}
 		return
 	}
 
-	if siteError && chatID != "" {
+	if siteError && adminChatID != "" {
 		siteError = false
+		msg := fmt.Sprintf("Сайт восстановился, текущий месяц: %s", newMonth)
 		if newMonth != lastMonth {
 			lastMonth = newMonth
-			sendTelegramMessage(botToken, chatID, fmt.Sprintf("Сайт восстановился, текущий месяц обновился до: %s", newMonth))
-		} else {
-			sendTelegramMessage(botToken, chatID, fmt.Sprintf("Сайт восстановился, текущий месяц: %s", newMonth))
+			msg = fmt.Sprintf("Сайт восстановился, текущий месяц обновился до: %s", newMonth)
 		}
+		sendTelegramMessage(botToken, adminChatID, msg)
 	} else if newMonth != lastMonth {
 		lastMonth = newMonth
-		if chatID != "" {
-			sendTelegramMessage(botToken, chatID, "Обнаружен новый месяц: "+newMonth)
+		if firstCheck {
+			firstCheck = false
+			if adminChatID != "" {
+				sendTelegramMessage(botToken, adminChatID, "Текущий месяц при старте: "+newMonth)
+			}
+		} else {
+			if chatID != "" {
+				sendTelegramMessage(botToken, chatID, "Обнаружен новый месяц: "+newMonth)
+			}
 		}
 	}
 }
